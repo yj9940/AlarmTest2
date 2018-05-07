45924551a11fa5a5a996bc20ccf0886266271e16
yj9940 <786436456@qq.com>
2018-05-07 09:06:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/alarmtest2/AlarmViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.lenovo.alarmtest2;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AlarmViewHolder {
+    TextView textView1;
+    TextView textView2;
+    ImageView image;
+}
